093b58a80349247587ae68b47be2995308699af8
DavidAkaFunky <futebol4@hotmail.com>
2023-10-05 18:15:04 +0100

    Add ex4 and ex5 (with some mistakes)

---
+++ src/ex4.sql
@@ -0,0 +1,38 @@
+SET NAMES utf8;
+
+DROP DATABASE IF EXISTS energy;
+
+CREATE DATABASE IF NOT EXISTS energy DEFAULT CHARACTER SET utf8;
+
+USE energy;
+
+DROP TABLE IF EXISTS energy_consumption;
+DROP TABLE IF EXISTS time;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS voltage;
+
+CREATE TABLE time (
+    time_id INTEGER PRIMARY KEY,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL
+);
+
+CREATE TABLE location (
+    location_id INTEGER PRIMARY KEY,
+    district VARCHAR(16) NOT NULL,
+    municipality VARCHAR(27) NOT NULL,
+    parish VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE voltage (
+    voltage_id INTEGER PRIMARY KEY,
+    level VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE energy_consumption (
+    id SERIAL,
+    time_id INTEGER NOT NULL REFERENCES time(time_id),
+    location_id INTEGER NOT NULL REFERENCES location(location_id),
+    voltage_id INTEGER NOT NULL REFERENCES voltage(voltage_id),
+    consumption NUMERIC(12, 3) NOT NULL
+);
