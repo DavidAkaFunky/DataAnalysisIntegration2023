cc8bf9dd391ab68cb250e2245ee015e74a3527fd
David Belchior <futebol4@hotmail.com>
2023-10-07 14:37:32 +0100

    Fix ex5 and add ex6

--- src/ex4.sql
+++ src/ex4.sql
@@ -14,19 +14,21 @@ DROP TABLE IF EXISTS voltage;
 CREATE TABLE time (
     time_id INTEGER PRIMARY KEY,
     year INTEGER NOT NULL,
-    month INTEGER NOT NULL
+    month INTEGER NOT NULL,
+    UNIQUE(year, month)
 );
 
 CREATE TABLE location (
     location_id INTEGER PRIMARY KEY,
     district VARCHAR(16) NOT NULL,
     municipality VARCHAR(27) NOT NULL,
-    parish VARCHAR(30) NOT NULL
+    parish VARCHAR(30) NOT NULL,
+    UNIQUE(district, municipality, parish)
 );
 
 CREATE TABLE voltage (
     voltage_id INTEGER PRIMARY KEY,
-    level VARCHAR(32) NOT NULL
+    level VARCHAR(32) NOT NULL UNIQUE
 );
 
 CREATE TABLE energy_consumption (
@@ -34,5 +36,6 @@ CREATE TABLE energy_consumption (
     time_id INTEGER NOT NULL REFERENCES time(time_id),
     location_id INTEGER NOT NULL REFERENCES location(location_id),
     voltage_id INTEGER NOT NULL REFERENCES voltage(voltage_id),
-    consumption NUMERIC(12, 3) NOT NULL
+    consumption NUMERIC(12, 3) NOT NULL,
+    UNIQUE(time_id, location_id, voltage_id)
 );
